5a957246c81aa8c9c05fc32a3c5141aee71d29a6
shashwatpandey100 <115147038+shashwatpandey100@users.noreply.github.com>
2023-08-30 21:09:44 +0530

    changing video src

--- src/app/components/video.jsx
+++ src/app/components/video.jsx
@@ -59,7 +59,7 @@ const Video = ({ videoOpen, setVideoOpen }) => {
                             controls={true}
                             className="bg-white rounded-3xl"
                         >
-                            <source src="https://download-video.akamaized.net/v2-1/playback/7ad19c57-5d6f-4b8c-bd9e-e26d0c2f389d/5c5a8e6d-aeb8599e?__token__=st=1693372212~exp=1693386612~acl=%2Fv2-1%2Fplayback%2F7ad19c57-5d6f-4b8c-bd9e-e26d0c2f389d%2F5c5a8e6d-aeb8599e%2A~hmac=057fc0493f6361685ad8dfec16af11a0ddb247ce05bdc9a594ecacee952831f3&r=dXMtY2VudHJhbDE%3D" type="video/mp4" />
+                            <source src="https://drive.google.com/uc?id=1oMaaIYo23heOatEu49HyFZDv3iw3Ml4H&export=download" type="video/mp4" />
                         </video>
                     }
                 </motion.div>
