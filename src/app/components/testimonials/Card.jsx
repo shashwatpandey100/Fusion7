3814cae1c17d04d2286c688a0103021309f323af
shashwatpandey100 <115147038+shashwatpandey100@users.noreply.github.com>
2023-11-20 14:31:05 +0530

    Update Card.jsx

--- src/app/components/testimonials/Card.jsx
+++ src/app/components/testimonials/Card.jsx
@@ -1,8 +1,15 @@
 import Star from "./Star";
 
 const Card = ({stars, text, name, position}) => {
+  const stylesP = {
+    '@media (min-width: 48rem)': {
+        fontSize: '1.67vw',
+        lineHeight: '1.3em',
+        marginBottom: '3.1vw',
+        marginTop: '5vw',
+    },
   return (
-    <div className="mx-[10px] responsiveWidth h-[630px] bg-[#e3e8ea] rounded-[25px] overflow-hidden cursor-pointer">
+    <div className="mx-[10px] responsiveWidth h-[85vh] bg-[#e3e8ea] rounded-[25px] overflow-hidden cursor-pointer">
       <div className="w-[100%] relative min-h-[400px] h-[100%] md:pt-[2.86vw] md:pr-[2.38vw] md:pb-[2.38vw] md:pl-[2.38vw] pt-[35px] pr-[30px] pb-[35px] pl-[30px] flex flex-col">
         <div className="bg-[#e3e8ea] h-[100%] w-[100%] flex flex-col justify-between">
           <div className="flex">
@@ -38,7 +45,7 @@ const Card = ({stars, text, name, position}) => {
             </div>
           </div>
           <div className="">
-            <p className="mb-[45px] mt-[45px] normalFont text-[20px]">
+            <p className="mb-[45px] mt-[45px] normalFont text-[20px]" style={stylesP}>
               {text}
             </p>
             <div>
