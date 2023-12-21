37c154aef90ec599d1ac0b78c81adda46e55b85e
shashwatpandey100 <115147038+shashwatpandey100@users.noreply.github.com>
2023-12-21 15:24:58 +0530

    contact section update

--- src/app/components/contactSection.jsx
+++ src/app/components/contactSection.jsx
@@ -64,7 +64,7 @@ const ContactSection = ({ openContact }) => {
   };
 
   return (
-    <section className="h-[100vh] w-[100vw] pt-[150px] relative overflow-hidden bg-[rgba(0,0,0,0.05)]">
+    <section className="h-[100vh] w-[100vw] mt-[150px] relative overflow-hidden bg-[rgba(0,0,0,0.05)]">
       <div className="h-full w-full absolute top-0 left-0 flex flex-col items-center justify-center">
         <div className="flex flex-col">
           <p className="text-[3em] text-center boldFont md:leading-[110px] lg:leading-[130px] md:text-[5em] lg:text-[8em] text-black uppercase">
